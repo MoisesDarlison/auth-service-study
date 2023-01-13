89cd02cd00e08ac6dabee11e2691400098880d5e
Darlison <moisesdarlison91@gmail.com>
2023-01-13 15:58:20 -0300

    refactor: mke SingUpController conforms to a protocol

--- src/presentation/controllers/sing-up.controller.ts
+++ src/presentation/controllers/sing-up.controller.ts
@@ -1,8 +1,9 @@
 import { MissingParamNames } from "../errors/missing-param.error";
 import { badRequest } from "../helpers/http.helper";
-import { HttpRequest, HttpResponse } from "../protocols/http";
+import { HttpRequest, HttpResponse } from "../protocols/http.protocol";
+import { Controller } from "../protocols/controller.protocol";
 
-export class SignUpController {
+export class SignUpController implements Controller {
   handle(httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ["email", "password", "passwordConfirmation"];
 

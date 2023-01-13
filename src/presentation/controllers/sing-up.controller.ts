3c1cc3db7ec69cc553ed4cb94ffb82fa971c5209
Darlison <moisesdarlison91@gmail.com>
2023-01-13 15:01:11 -0300

    feat: ensure SingUpController returns statusCode 400 and an error if no password provider

--- src/presentation/controllers/sing-up.controller.ts
+++ src/presentation/controllers/sing-up.controller.ts
@@ -1,5 +1,7 @@
 export class SignUpController {
   handle(httpRequest: any): any {
-    return { statusCode: 400, body: new Error("Missing Params: email") };
+    if (!httpRequest.body.email)
+      return { statusCode: 400, body: new Error("Missing Params: email") };
+    return { statusCode: 400, body: new Error("Missing Params: password") };
   }
 }

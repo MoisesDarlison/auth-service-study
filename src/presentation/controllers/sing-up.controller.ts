f230c8d1c4c80fa01075290e280c46489d956f8a
Darlison <moisesdarlison91@gmail.com>
2023-01-13 15:26:59 -0300

    refactor: ensure singUpController.handle implement correct interfaces

--- src/presentation/controllers/sing-up.controller.ts
+++ src/presentation/controllers/sing-up.controller.ts
@@ -1,5 +1,7 @@
+import { HttpRequest, HttpResponse } from "../protocols/http";
+
 export class SignUpController {
-  handle(httpRequest: any): any {
+  handle(httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.email)
       return { statusCode: 400, body: new Error("Missing Params: email") };
     return { statusCode: 400, body: new Error("Missing Params: password") };

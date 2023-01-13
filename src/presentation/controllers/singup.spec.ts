c1f73e985dee938a7f990cd1d9323cf33ed61571
Darlison <moisesdarlison91@gmail.com>
2023-01-13 14:47:48 -0300

    test: ensure SingUpController returns statusCode 400 if no email provider

---
+++ src/presentation/controllers/singup.spec.ts
@@ -0,0 +1,16 @@
+import { SignUpController } from "./sing-up.controller";
+
+describe("SignUp Controller", () => {
+  it("should return 400 if no email is provided", () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        password: "password_test",
+        passwordConfirmation: "password_test",
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+  });
+});

12f5adf6e7610e4df15f6912d9fec52c3cfb785c
Darlison <moisesdarlison91@gmail.com>
2023-01-13 17:28:13 -0300

    refactor: move EmailValidator error to mokeImplements

--- src/presentation/controllers/singup.spec.ts
+++ src/presentation/controllers/singup.spec.ts
@@ -15,15 +15,6 @@ const makeEmailValidator = () => {
   return new EmailValidatorStub();
 };
 
-const makeEmailValidatorWithError = () => {
-  class EmailValidatorStub implements EmailValidator {
-    isValid(email: string): boolean {
-      throw new Error();
-    }
-  }
-  return new EmailValidatorStub();
-};
-
 const makeSut = (): SutTypes => {
   const emailValidatorStub = makeEmailValidator();
   const sut = new SignUpController(emailValidatorStub);
@@ -110,7 +101,10 @@ describe("SignUp Controller", () => {
   });
 
   it("Should return 500 if emailValidator throw", () => {
-    const emailValidatorStub = makeEmailValidatorWithError();
+    const { emailValidatorStub } = makeSut();
+    jest.spyOn(emailValidatorStub, "isValid").mockImplementationOnce(() => {
+      throw new Error();
+    });
     const sut = new SignUpController(emailValidatorStub);
 
     const httpRequest = {

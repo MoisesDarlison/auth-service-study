65cc530acad44646bb82f888720e50075d371ba6
Darlison <moisesdarlison91@gmail.com>
2023-01-15 15:23:57 -0300

    test: ensure EmailValidatorAdapter calls with correct email

--- src/utils/email-validator.adapter.spec.ts
+++ src/utils/email-validator.adapter.spec.ts
@@ -7,27 +7,30 @@ jest.mock("email-validator", () => ({
   },
 }));
 
-interface SutTypes {
-  sut: EmailValidatorAdapter;
-}
-
-const makeSut = (): SutTypes => {
-  const sut = new EmailValidatorAdapter();
-  return { sut };
+const makeSut = (): EmailValidatorAdapter => {
+  return new EmailValidatorAdapter();
 };
 
 describe("Email Validator Adapter", () => {
   it("Should return FALSE if validator return FALSE", () => {
-    const { sut } = makeSut();
-
+    const sut = makeSut();
     jest.spyOn(emailValidator, "validate").mockReturnValueOnce(false);
     const isValid = sut.isValid("test@email.com");
     expect(isValid).toBe(false);
   });
 
   it("Should return TRUE if validator return TRUE", () => {
-    const { sut } = makeSut();
+    const sut = makeSut();
     const isValid = sut.isValid("test@email.com");
     expect(isValid).toBe(true);
+  });
+
+  it("Should call emailValidator with correct email", () => {
+    const sut = makeSut();
+
+    const isEmailSpy = jest.spyOn(emailValidator, "validate");
+
+    sut.isValid("test@email.com");
+    expect(isEmailSpy).toHaveBeenCalledWith("test@email.com");
   });
 });

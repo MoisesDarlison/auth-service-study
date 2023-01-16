2b550394640525d0650eab4f6336852bfd839910
Darlison <moisesdarlison91@gmail.com>
2023-01-16 18:32:57 -0300

    test: ensure singUpRoute with factory and adapter to desencap to express framework

--- src/main/routes/signup.route.test.ts
+++ src/main/routes/signup.route.test.ts
@@ -1,7 +1,26 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
 import request from "supertest";
+import { MongoHelper } from "../../infra/helpers/mongo.helper";
 import { app } from "../config/app";
 
 describe("SignUp Route", () => {
+  let mongoMemoryServer: MongoMemoryServer;
+
+  beforeAll(async () => {
+    mongoMemoryServer = await MongoMemoryServer.create();
+    await MongoHelper.connect(mongoMemoryServer.getUri());
+  });
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection("accounts");
+    await accountCollection.deleteMany({});
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+    await mongoMemoryServer.stop();
+  });
+
   it("Should return an account on success", async () => {
     await request(app)
       .post("/v1/api/signup")
@@ -9,7 +28,7 @@ describe("SignUp Route", () => {
         nickName: "Test",
         email: "test@email.com",
         password: "password",
-        confirmationPassword: "password",
+        passwordConfirmation: "password",
       })
       .expect(200);
   });

cb09e21d1cfdbde837ed46a50a118f38af14ed88
Darlison <moisesdarlison91@gmail.com>
2023-01-16 12:00:08 -0300

    test: ensure return content-type HTML forcerd

--- src/main/middlewares/content-type.test.ts
+++ src/main/middlewares/content-type.test.ts
@@ -7,7 +7,19 @@ describe("Content Type Middleware", () => {
 
     await request(app)
       .get("/test_content_type")
-      .send()
+      .send("")
       .expect("content-type", /json/);
+  });
+
+  it("Should return content type as HTML forced", async () => {
+    app.get("/test_content_type_html", (req, res) => {
+      res.type("html");
+      res.send("");
+    });
+
+    await request(app)
+      .get("/test_content_type_html")
+      .send()
+      .expect("content-type", /html/);
   });
 });

1897b352c0f35019bcfd012a682744226818c090
Darlison <moisesdarlison91@gmail.com>
2023-01-18 17:07:59 -0300

    feat: ensure reconnect in database if database down

--- src/infra/helpers/mongo.helper.ts
+++ src/infra/helpers/mongo.helper.ts
@@ -2,6 +2,7 @@ import { Collection, MongoClient, ObjectId } from "mongodb";
 
 export const MongoHelper = {
   client: null as unknown as MongoClient,
+  hasConnected: false,
   async connect(uri: string) {
     this.client = new MongoClient(uri);
     await this.client.connect();
@@ -10,8 +11,20 @@ export const MongoHelper = {
     await this.client.close();
   },
 
-  getCollection(dbName: string): Collection {
-    return this.client.db("AuthService").collection(dbName);
+  async getCollection(dbName: string): Promise<Collection> {
+    /**
+     * I inserted a tryCatch at this point as the isConnected attribute is no longer available for this version of mongoDB.
+     * And from what I've read, the same is making this reconnection attempt internally.
+     * but I prefer to maintain a treatment at this point as well.
+     **/
+
+    try {
+      await this.client.db().stats();
+      return this.client.db("AuthService").collection(dbName);
+    } catch (error) {
+      await this.client.connect();
+      return this.client.db("AuthService").collection(dbName);
+    }
   },
 
   map(collection: any, id: ObjectId): any {

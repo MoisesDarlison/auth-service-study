ca5efe2ed713ed2a73a92c8b6672c7427d25cb97
Darlison <moisesdarlison91@gmail.com>
2023-01-20 18:17:34 -0300

    docs: add TODO information on AddLogErrorRepository

--- src/infra/db/in-files/log-error.ts
+++ src/infra/db/in-files/log-error.ts
@@ -2,6 +2,7 @@ import { IAddLogErrorRepository } from "../../../data/protocols/add-log-error.re
 
 export class AddLogErrorRepository implements IAddLogErrorRepository {
   async log(stack: string): Promise<void> {
+    //TODO: Implements save on Files
     console.log({ Message: "Save on TXT", stack: stack });
   }
 }
